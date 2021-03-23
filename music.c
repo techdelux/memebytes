233a1325cc92fd96234e7518698f9f5e699c13d2
techdelux <handcraftedtechnologies@gmail.com>
2021-03-23 00:22:41 -0700

    mido2freq/001**

--- music.c
+++ music.c
@@ -9,13 +9,29 @@ void midifreq()
 // function.
 {
 	double semitone_ratio;
-	double c0; /* for frequency of MIDI Note 0 */
-	double c5; /* for frquency of Middle C */
-	double frequency; /* . . . which we want to find. */
-	int midinote; /* . . . given this note. */
+	// for frequency of MIDI Note 0.
+	double c0;
+	 // for frequency of Middle C.
+	double c5;
+	// . . . which we want to find.
+	double frequency;
+	// . . . given this note.
+	int midinote;
 
 	/* calculate required numbers */
-	printf("midifreq/001\n");
+	semitone_ratio = pow(2, 1/12.0);
+
+	c5 = 220.0 * pow(semitone_ratio, 3);
+	c0 = c5 * pow(0.5, 5);
+
+	// Calculate freq for given MIDI Note.
+	midinote = 73;
+	frequency = c0 * pow(semitone_ratio, midinote);
+	printf("MIDI Note %d" midinote);
+
+
+	printf("midifreq/001**\n");
+
 
 
 }
@@ -60,4 +76,20 @@ int main(){
 // frequency change by a factor (or 
 // ratio) of 2: doubled for an octave
 // rise, and halved for an octave fall.
+// 
+// So an octave below Concert A would
+// have the frequency 220 Hz & a further
+// octave fall halves the frequency to
+// 110 Hz.
 //
+// A single semitone rise corresponds to
+// the (approximate) value 1.0594631,
+// which is (approximately) the twelfth
+// root of 2 (a semi-tone "only" in E.T.
+// 12 tuning). That is to say, multiply
+// 1.0594631 by itself twelve times and
+// you should get the value 2.0.
+//
+// This operation, known as raisong to a
+// power, is notaded mathematically as
+// 1.0594631^12.
