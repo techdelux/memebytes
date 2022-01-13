bf0e095f7b40778d16b9c14d0753770b64bcf196
techdelux <handcraftedtechnologies@gmail.com>
2022-01-12 23:03:02 -0600

    lounge/005

--- memebytes/lib/main.dart
+++ memebytes/lib/main.dart
@@ -1,1 +1,31 @@
-// lounge/004
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:memebytes/responsive/mobileLayout.dart';
+import 'package:memebytes/responsive/responsiveScreen.dart';
+import 'package:memebytes/responsive/webLayout.dart';
+import 'package:memebytes/utils/color.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const Memebytes());
+}
+
+class Memebytes extends StatelessWidget {
+  const Memebytes({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'memebytes',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout: WebScreenLayout(),
+      ),
+    );
+  }
+}
